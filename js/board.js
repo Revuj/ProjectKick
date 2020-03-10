86c14445344577c8328596e6daaf1b2db8c25976
JoaoABelha <jpabelha@hotmail.com>
2020-03-10 19:29:05 +0000

    filters in boards

--- js/board.js
+++ js/board.js
@@ -62,15 +62,15 @@ create_list_btn.addEventListener("click", () => {
   listenAddItem(addItemButtons[addItemButtons.length - 2]);
   listenCancelAddItem(addItemButtons[addItemButtons.length - 1]);
 
-  newList.addEventListener("dragover", function(e) {
+  newList.addEventListener("dragover", function (e) {
     e.preventDefault();
   });
 
-  newList.addEventListener("dragenter", function(e) {
+  newList.addEventListener("dragenter", function (e) {
     e.preventDefault();
   });
 
-  newList.children[1].addEventListener("drop", function(e) {
+  newList.children[1].addEventListener("drop", function (e) {
     console.log(newList);
     this.append(draggedItem);
   });
@@ -122,7 +122,7 @@ function listenCancelAddItem(elem) {
 [...cancel_add_item_button].forEach(elem => listenCancelAddItem(elem));
 
 /* Allows modal to know which list to delete */
-$("#delete-list-modal").on("show.bs.modal", function(event) {
+$("#delete-list-modal").on("show.bs.modal", function (event) {
   let button = $(event.relatedTarget); // Button that triggered the modal
   let recipient = button.data("list-id"); // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
@@ -142,15 +142,15 @@ delete_list_button.addEventListener("click", event => {
 });
 
 function setDraggable(elem) {
-  elem.addEventListener("dragstart", function(e) {
+  elem.addEventListener("dragstart", function (e) {
     draggedItem = elem;
-    setTimeout(function() {
+    setTimeout(function () {
       elem.style.display = "none";
     }, 0);
   });
 
-  elem.addEventListener("dragend", function(e) {
-    setTimeout(function() {
+  elem.addEventListener("dragend", function (e) {
+    setTimeout(function () {
       draggedItem.style.display = "block";
       draggedItem = null;
     }, 0);
@@ -159,15 +159,15 @@ function setDraggable(elem) {
   for (let j = 0; j < lists.length; j++) {
     const list = lists[j];
 
-    list.addEventListener("dragover", function(e) {
+    list.addEventListener("dragover", function (e) {
       e.preventDefault();
     });
 
-    list.addEventListener("dragenter", function(e) {
+    list.addEventListener("dragenter", function (e) {
       e.preventDefault();
     });
 
-    list.addEventListener("drop", function(e) {
+    list.addEventListener("drop", function (e) {
       console.log("oi");
       console.log(this);
       this.append(draggedItem);
@@ -188,7 +188,7 @@ function dragDrop() {
   })
 );
 
-$("#edit-task-modal").on("show.bs.modal", function(event) {
+$("#edit-task-modal").on("show.bs.modal", function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
   var recipient = button.data("task-id"); // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
@@ -263,7 +263,7 @@ cancelEditTitleButton.addEventListener("click", event => {
 });
 
 [...sideIssueButtons].forEach(elem =>
-  elem.addEventListener("click", function() {
+  elem.addEventListener("click", function () {
     let taskID = elem.getAttribute("id");
     let taskTitle = elem.querySelector(".task-title").innerHTML;
     if (pageWrapper.classList.contains("is-collapsed-right")) {
@@ -311,3 +311,186 @@ function changecolors(type_message, item) {
 
   setTimeout(change.bind(null, item, "#fff"), 1000);
 }
+
+/*========================  FILTERS ==================================*/
+const input_filter = document.querySelector('#filter-issues')
+
+input_filter.addEventListener('input', (e) => {
+  filterTasks(tasks_list, e.target.value)
+
+})
+
+const tasks_list = [
+  {
+    name: 'Backlog',
+    tasks: [
+      {
+        id: 1,
+        title: 'Work assigned to me',
+        list_counter: 1,
+        creator: 'Revuj',
+        tags: ['Iteration1'],
+        imgCreator: 'https://avatars3.githubusercontent.com/u/41621540?s=40&v=4'
+      },
+      {
+        id: 2,
+        title: 'This is another',
+        list_counter: 2,
+        creator: 'Jpabelha',
+        tags: ['Iteration2'],
+        imgCreator: 'https://avatars2.githubusercontent.com/u/44231794?s=40&v=4'
+      }
+    ] // end of tasks 1
+  }, // first task list
+
+  {
+    name: 'Develop',
+    tasks: [
+      {
+        id: 3,
+        title: 'Work assigned to me',
+        list_counter: 1,
+        creator: 'Revuj',
+        tags: ['Iteration1', 'Dope'],
+        imgCreator: 'https://avatars3.githubusercontent.com/u/41621540?s=40&v=4'
+      },
+      {
+        id: 4,
+        title: 'This is another',
+        list_counter: 2,
+        creator: 'Jpabelha',
+        tags: ['Iteration2'],
+        imgCreator: 'https://avatars2.githubusercontent.com/u/44231794?s=40&v=4'
+      }
+    ]
+  }
+
+
+]
+
+
+function filterTasks(task_list, value) {
+
+  let filtered = []
+
+  task_list.forEach(task => { // for each board
+
+    let element = { name: task.name }
+
+
+    element.tasks = task.tasks.filter(element => { // for each task
+      const regex = new RegExp(`^${value}`, 'gi');
+      
+      // filter creator
+      if(element.creator.match(regex)) return true;
+
+      //filter title
+      if(element.title.match(regex)) return true;
+
+      // filter by tags
+      let tags = element.tags;
+      for(let i = 0; i < tags.length; i++) {
+        if (tags[i].match(regex)) return true;
+      }
+      return false;
+    })
+
+    filtered.push(element);
+  })
+
+
+  kanban_table.innerHTML = outputKanbanHTML(filtered);
+}
+
+function outputKanbanHTML(tasks) {
+
+  let output = ''
+  for (let i = 0; i < tasks.length; i++) {
+    output += outputTaskListHTML(tasks[i])
+  }
+  return output;
+}
+
+
+function outputTaskListHTML(tasksJson) {
+
+  let tasks_list = '';
+  tasksJson.tasks.forEach(task => {
+    let tags = '';
+
+    /*maybe in the future put here some limit */
+    task.tags.forEach(tag => {
+      tags+= `
+        <h6 class="mb-0 p-1 list-item-label bg-info ml-1">
+        ${tag}
+      </h6>
+      `
+    })
+
+    tasks_list += `
+    <li id=${task.id} class="task-item text-left" draggable="true">
+      <div class="d-flex flex-row align-items-center ml-2 row-1">
+        <h6 class="mb-0 py-2 task-title">${task.title}</h6>
+        <button type="button" class="btn ml-auto d-none edit-task" data-toggle="modal"
+          data-target="#edit-task-modal" data-task-id="1">
+          <i class="fas fa-pencil-alt float-right"></i>
+        </button>
+      </div>
+      <span class="d-flex flex-row align-items-center mx-2 row-2">
+        <p class="w-100 mb-2">
+          <span class="list-item-counter">#${task.list_counter}</span>
+          <span class="list-item-creator"> Opened by ${task.creator}</span>
+        </p>
+      </span>
+      <span class="d-flex flex-row align-items-center mx-2 row-3">
+        ${tags}
+        <span class="list-item-author ml-auto"><img
+            src="${task.imgCreator}" alt="@${task.creator}"
+            draggable="false" />
+        </span>
+      </span>
+    </li>
+    `
+  });
+
+
+  tasks_list += `
+  <li class="add-item-li collapse" id="add-item-${tasksJson.name}">
+    <form class="add-item-form form-group">
+      <div class="form-group text-left">
+        <label for="item-title">Titletool</label>
+        <input type="text" class="form-control" class="item-title" placeholder="" />
+      </div>
+      <div class="d-flex justify-content-between">
+        <button type="submit" class="btn btn-primary add-item btn">Submit</button>
+        <button type="reset" class="btn btn-primary cancel-add-item">Cancel</button>
+      </div>
+    </form>
+  </li>
+  `
+  let tasks = ` <ul class="task-items">${tasks_list}</ul>`
+
+  const output = `
+  <div class="bd-highlight task" id="task-list-${tasksJson.name}">
+    <div class="task-list-title d-flex align-items-center py-0">
+      <h6 class="mr-auto my-0 text-left p-3">
+        <i class="fa fa-fw fa-caret-right"></i>${tasksJson.name}
+      </h6>
+      <button class="btn mx-4 p-0 order-3" data-toggle="collapse" data-target="#add-item-${tasksJson.name}"
+        aria-expanded="false" aria-controls="add-item">
+        <i class="fas fa-plus"></i>
+      </button>
+      <button type="button" class="btn" data-toggle="modal" data-target="#delete-list-modal"
+       data-list-id="task-list-${tasksJson.name}">
+       <i class="fas fa-trash-alt"></i>
+      </button>
+    </div>
+    ${tasks}
+  </div>
+  `;
+  return output;
+}
+
+
+kanban_table.innerHTML = outputKanbanHTML(tasks_list)
+
