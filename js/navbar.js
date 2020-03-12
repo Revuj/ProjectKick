9a057ea0804863a27ce6358448b49af2f7611b4a
Revuj <revuj112@gmail.com>
2020-03-12 17:13:34 +0000

    Added navbar.js and profile dropdown to all pages

---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+
+let user_dropdown = document.querySelector('.user-dropdown');
+let notification_dropdown = document.querySelector('.notification-dropdown');
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+    document.querySelector('.page-wrapper').classList.toggle('is-collapsed');
+})
+
+document.querySelector('.notification').addEventListener('click', function () {
+    notification_dropdown.classList.toggle('d-none');
+    if (!user_dropdown.classList.contains("d-none"))
+        user_dropdown.classList.add("d-none")
+})
+
+document.querySelector('.user').addEventListener('click', function () {
+    user_dropdown.classList.toggle('d-none');
+    if (!notification_dropdown.classList.contains("d-none"))
+        notification_dropdown.classList.add("d-none")
+})
