9f34d3b8d5b01dde66dcfa01e53cd39ebcc9ed3c
up201703591 <up201703591@fe.up.pt>
2020-03-20 12:09:14 +0000

    falta ver o chat

--- js/board.js
+++ js/board.js
@@ -60,15 +60,15 @@ create_list_btn.addEventListener("click", () => {
   listenAddItem(addItemButtons[addItemButtons.length - 2]);
   listenCancelAddItem(addItemButtons[addItemButtons.length - 1]);
 
-  newList.addEventListener("dragover", function (e) {
+  newList.addEventListener("dragover", function(e) {
     e.preventDefault();
   });
 
-  newList.addEventListener("dragenter", function (e) {
+  newList.addEventListener("dragenter", function(e) {
     e.preventDefault();
   });
 
-  newList.children[1].addEventListener("drop", function (e) {
+  newList.children[1].addEventListener("drop", function(e) {
     console.log(newList);
     this.append(draggedItem);
   });
@@ -116,7 +116,7 @@ function listenCancelAddItem(elem) {
 }
 
 /* Allows modal to know which list to delete */
-$("#delete-list-modal").on("show.bs.modal", function (event) {
+$("#delete-list-modal").on("show.bs.modal", function(event) {
   let button = $(event.relatedTarget); // Button that triggered the modal
   let recipient = button.data("list-id"); // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
@@ -136,18 +136,18 @@ delete_list_button.addEventListener("click", event => {
 });
 
 function setDraggable(elem) {
-  elem.addEventListener("dragstart", function (e) {
+  elem.addEventListener("dragstart", function(e) {
     e.stopPropagation();
     console.log(elem);
 
     draggedItem = elem;
-    setTimeout(function () {
+    setTimeout(function() {
       elem.style.display = "none";
     }, 0);
   });
 
-  elem.addEventListener("dragend", function (e) {
-    setTimeout(function () {
+  elem.addEventListener("dragend", function(e) {
+    setTimeout(function() {
       draggedItem.style.display = "block";
       draggedItem = null;
     }, 0);
@@ -157,15 +157,15 @@ function setDraggable(elem) {
   for (let j = 0; j < lists.length; j++) {
     const list = lists[j];
 
-    list.addEventListener("dragover", function (e) {
+    list.addEventListener("dragover", function(e) {
       e.preventDefault();
     });
 
-    list.addEventListener("dragenter", function (e) {
+    list.addEventListener("dragenter", function(e) {
       e.preventDefault();
     });
 
-    list.addEventListener("drop", function (e) {
+    list.addEventListener("drop", function(e) {
       console.log("oi");
       console.log(this);
       this.append(draggedItem);
@@ -206,7 +206,7 @@ function mouseLeaveListItem(elem) {
 }
 
 function openSideIssueListen(elem) {
-  elem.addEventListener("click", function () {
+  elem.addEventListener("click", function() {
     let taskID = elem.getAttribute("id");
     let taskTitle = elem.querySelector(".task-title").innerHTML;
     if (pageWrapper.classList.contains("is-collapsed-right")) {
@@ -381,7 +381,7 @@ const tasks_list = [
         creator: "Jpabelha",
         tags: ["Iteration2"],
         imgCreator: "https://avatars2.githubusercontent.com/u/44231794?s=40&v=4"
-      },
+      }
     ] // end of tasks 1
   }, // first task list
 
@@ -409,20 +409,17 @@ const tasks_list = [
 
   {
     name: "Doing",
-    tasks: [
-    ]
+    tasks: []
   },
 
   {
     name: "Needing bug fixed",
-    tasks: [
-    ]
+    tasks: []
   },
 
   {
     name: "Closed",
-    tasks: [
-    ]
+    tasks: []
   }
 ];
 
@@ -489,7 +486,7 @@ function outputTaskListHTML(tasksJson) {
     tasks_list += `
     <li id=${task.id} class="task-item text-left" draggable="true">
       <div class="d-flex flex-row align-items-center ml-2 row-1">
-        <h6 class="mb-0 py-2 task-title">${task.title}</h6>
+        <h6 class="mb-0 py-2 task-title title">${task.title}</h6>
         <button type="button" class="btn ml-auto d-none edit-task">
           <i class="fas fa-pencil-alt float-right"></i>
         </button>
--- js/notifications.js
+++ js/notifications.js
@@ -1,85 +1,135 @@
-let notifications_filters = document.querySelectorAll("#notifications-list-container li");
+let notifications_filters = document.querySelectorAll(
+  "#notifications-list-container li"
+);
 
 /* fecth from the database */
 let messages = [
-  { type: 'invited', sender: 'John', date: 'just now', project: "lbaw2025" },
-  { type: 'invited', sender: 'Abelha', date: '2 days ago', project: "sdis" },
-  { type: 'invited', sender: 'Abelha', date: '1 month ago', project: "iart-proj" },
-  { type: 'kicked', sender: 'Tiago', date: '3 days ago', project: "ltw" },
-  { type: 'assigned', date: '1 minute ago', project: "BDAD", title: "Delivery 1st Project" },
-  { type: 'assigned', date: '1 day ago', project: "BDAD", title: "Normalize database" },
-  { type: 'assigned', date: '2 weeks ago', project: "PPIN", title: "Namaste" },
-  { type: 'issue', date: '1 minute ago', project: "LPOO", title: "Mutation Tests" },
-  { type: 'meeting', date: '1 week ago', project: "LPOO" } // system messages and reports to the admin
+  { type: "invited", sender: "John", date: "just now", project: "lbaw2025" },
+  { type: "invited", sender: "Abelha", date: "2 days ago", project: "sdis" },
+  {
+    type: "invited",
+    sender: "Abelha",
+    date: "1 month ago",
+    project: "iart-proj"
+  },
+  { type: "kicked", sender: "Tiago", date: "3 days ago", project: "ltw" },
+  {
+    type: "assigned",
+    date: "1 minute ago",
+    project: "BDAD",
+    title: "Delivery 1st Project"
+  },
+  {
+    type: "assigned",
+    date: "1 day ago",
+    project: "BDAD",
+    title: "Normalize database"
+  },
+  { type: "assigned", date: "2 weeks ago", project: "PPIN", title: "Namaste" },
+  {
+    type: "issue",
+    date: "1 minute ago",
+    project: "LPOO",
+    title: "Mutation Tests"
+  },
+  { type: "meeting", date: "1 week ago", project: "LPOO" } // system messages and reports to the admin
 ];
 
-notifications_filters.forEach(elem => elem.addEventListener("click", event => {
-  notifications_filters.forEach(elem => elem.classList.remove("active"));
-  elem.classList.add("active");
-  let notifications_container = document.querySelector('.notification-container');
-  notifications_container.innerHTML = '';
-  let filtered_messages;
-  switch (elem.id) {
-    case "assigned-notifications":
-      filtered_messages = messages.filter(message => message.type == 'assigned')
-      break;
-    case "invited-notifications":
-      filtered_messages = messages.filter(message => message.type == 'invited')
-      break;
-    case "meetings-notifications":
-      filtered_messages = messages.filter(message => message.type == 'meeting')
-      break;
-    case "others-notifications":
-      filtered_messages = messages.filter(message => (message.type == 'kicked' || message.type == 'issue'))
-      break;
-    case "all-notifications":
-      filtered_messages = messages;
-      break;
-  }
-  renderMessages(filtered_messages)
-}))
+notifications_filters.forEach(elem =>
+  elem.addEventListener("click", event => {
+    notifications_filters.forEach(elem => elem.classList.remove("active"));
+    elem.classList.add("active");
+    let notifications_container = document.querySelector(
+      ".notification-container"
+    );
+    notifications_container.innerHTML = "";
+    let filtered_messages;
+    switch (elem.id) {
+      case "assigned-notifications":
+        filtered_messages = messages.filter(
+          message => message.type == "assigned"
+        );
+        break;
+      case "invited-notifications":
+        filtered_messages = messages.filter(
+          message => message.type == "invited"
+        );
+        break;
+      case "meetings-notifications":
+        filtered_messages = messages.filter(
+          message => message.type == "meeting"
+        );
+        break;
+      case "others-notifications":
+        filtered_messages = messages.filter(
+          message => message.type == "kicked" || message.type == "issue"
+        );
+        break;
+      case "all-notifications":
+        filtered_messages = messages;
+        break;
+    }
+    renderMessages(filtered_messages);
+  })
+);
 
 function renderMessages(messages) {
   messages.forEach(message => {
     let msg;
     switch (message.type) {
-      case 'invited':
-        msg = new invited(message.type, message.sender, message.date, message.project);
+      case "invited":
+        msg = new invited(
+          message.type,
+          message.sender,
+          message.date,
+          message.project
+        );
         break;
-      case 'kicked':
-        msg = new kicked(message.type, message.sender, message.date, message.project);
+      case "kicked":
+        msg = new kicked(
+          message.type,
+          message.sender,
+          message.date,
+          message.project
+        );
         break;
-      case 'assigned':
-        msg = new assigned(message.type, message.date, message.project, message.title);
+      case "assigned":
+        msg = new assigned(
+          message.type,
+          message.date,
+          message.project,
+          message.title
+        );
         break;
-      case 'issue':
-        msg = new issue(message.type, message.date, message.project, message.title);
+      case "issue":
+        msg = new issue(
+          message.type,
+          message.date,
+          message.project,
+          message.title
+        );
         break;
-      case 'meeting':
+      case "meeting":
         msg = new meeting(message.type, message.date, message.project);
         break;
-      default: return;
-
+      default:
+        return;
     }
 
     msg.render();
-
-  })
+  });
 }
 
-
 class message {
-
   constructor(type, date) {
     this.type = type;
     //this.date = this.timestampToDate(date);
     this.date = date;
-    this.notification_list = document.querySelector('.notification-container');
-
+    this.notification_list = document.querySelector(".notification-container");
   }
 
   render() {
-    throw new Error('You have to implement the method render!');
+    throw new Error("You have to implement the method render!");
   }
 
   timestampToDate(dateArg) {
@@ -88,16 +138,21 @@ class message {
     let minutes = "0" + date.getMinutes();
     let seconds = "0" + date.getSeconds();
 
-    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    let formattedTime =
+      hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
     return formattedTime;
   }
 
   /*to add generic classes */
   addClassesToElementList(contentTemplate) {
-    contentTemplate.classList.add("m-2", "p-2", "notification-list-item", "border-bottom");
+    contentTemplate.classList.add(
+      "m-2",
+      "p-2",
+      "notification-list-item",
+      "border-bottom"
+    );
   }
 }
-
 
 class invited extends message {
   constructor(type, sender, date, project) {
@@ -107,30 +162,27 @@ class invited extends message {
   }
 
   render() {
-    let contentTemplate = document.createElement('li');
+    let contentTemplate = document.createElement("li");
     contentTemplate.classList.add("invited-notification");
     this.addClassesToElementList(contentTemplate);
 
-    let upperContent = document.createElement('div')
-    upperContent.classList.add('d-flex', 'justify-content-between');
-    upperContent.innerHTML +=
-      `<div class = "d-flex align-items-center">
+    let upperContent = document.createElement("div");
+    upperContent.classList.add("d-flex", "justify-content-between");
+    upperContent.innerHTML += `<div class = "d-flex align-items-center">
                 <img class = "m-2" src="./assets/profile.png" alt="profile_pic" style="width: 40px">
                 <p><span class="author-reference">${this.sender} </span>invited you to the project <span class="project-reference">${this.project}</span></p>
             </div>
             <p class="timestamp smaller-text m-2">${this.date}</p>`;
 
-    contentTemplate.appendChild(upperContent)
+    contentTemplate.appendChild(upperContent);
 
-    contentTemplate.innerHTML += `<button type="submit" class="custom-button primary-button mx-2">Accept <i class="fas fa-check"></i></button>`
-    contentTemplate.innerHTML += `<button type="submit" class="custom-button secondary-button mx-2">Deny <i class="fas fa-times"></i></button>`
+    contentTemplate.innerHTML += `<button type="submit" class="custom-button primary-button mx-2">Accept <i class="fas fa-check"></i></button>`;
+    contentTemplate.innerHTML += `<button type="submit" class="custom-button secondary-button mx-2">Deny <i class="fas fa-times"></i></button>`;
     this.notification_list.appendChild(contentTemplate);
-
   }
 }
 
 class kicked extends message {
-
   constructor(type, sender, date, project) {
     super(type, date);
     this.sender = sender;
@@ -138,21 +190,19 @@ class kicked extends message {
   }
 
   render() {
-
-    let contentTemplate = document.createElement('li');
+    let contentTemplate = document.createElement("li");
     contentTemplate.classList.add("kicked-notification");
     this.addClassesToElementList(contentTemplate);
 
-    let upperContent = document.createElement('div')
-    upperContent.classList.add('d-flex', 'justify-content-between');
-    upperContent.innerHTML +=
-      `<div class = "d-flex align-items-center justify-content-center">
+    let upperContent = document.createElement("div");
+    upperContent.classList.add("d-flex", "justify-content-between");
+    upperContent.innerHTML += `<div class = "d-flex align-items-center justify-content-center">
                 <img class = "m-2" src="./assets/profile.png" alt="profile_pic" style="width: 40px">
                 <p><span class="author-reference">${this.sender} </span>kicked you out of the project <span class="project-reference">${this.project}</span>
             </div>
             <p class="timestamp smaller-text m-2">${this.date}</p>`;
 
-    contentTemplate.appendChild(upperContent)
+    contentTemplate.appendChild(upperContent);
 
     this.addClassesToElementList(contentTemplate);
     this.notification_list.appendChild(contentTemplate);
@@ -167,20 +217,18 @@ class assigned extends message {
   }
 
   render() {
-
-    let contentTemplate = document.createElement('li');
+    let contentTemplate = document.createElement("li");
     contentTemplate.classList.add("assigned-notification");
-    let upperContent = document.createElement('div')
-    upperContent.classList.add('d-flex', 'justify-content-between');
+    let upperContent = document.createElement("div");
+    upperContent.classList.add("d-flex", "justify-content-between");
     upperContent.innerHTML += `<p> The issue <span class="issue-reference">${this.title}</span> on the project <span class="project-reference">${this.project}</span> has been assigned to you</p>
                                 <p class="timestamp smaller-text">${this.date}</p>`;
 
-    contentTemplate.appendChild(upperContent)
-    contentTemplate.innerHTML += `<a href="issue.html"><button type="submit" class="custom-button primary-button mx-2">Go to Issue</button></a>`
+    contentTemplate.appendChild(upperContent);
+    contentTemplate.innerHTML += `<a href="issue.html"><button type="submit" class="custom-button primary-button mx-2">Go to Issue</button></a>`;
     this.notification_list.appendChild(contentTemplate);
     this.addClassesToElementList(contentTemplate);
     this.notification_list.appendChild(contentTemplate);
-
   }
 }
 
@@ -192,20 +240,18 @@ class issue extends message {
   }
 
   render() {
-
-    let contentTemplate = document.createElement('li');
+    let contentTemplate = document.createElement("li");
     contentTemplate.classList.add("issue-notification");
-    let upperContent = document.createElement('div')
-    upperContent.classList.add('d-flex', 'justify-content-between');
+    let upperContent = document.createElement("div");
+    upperContent.classList.add("d-flex", "justify-content-between");
     upperContent.innerHTML += `<p><i class="fas fa-issues"></i> The issue <span class="issue-reference">${this.title}</span> on the project <span class="project-reference">${this.project}</span> is reaching it's deadline</p>
                                 <p class="timestamp smaller-text">${this.date}</p>`;
 
-    contentTemplate.appendChild(upperContent)
-    contentTemplate.innerHTML += `<a href="issue.html"><button type="submit" class="custom-button primary-button mx-2">Go to Issue</button></a>`
+    contentTemplate.appendChild(upperContent);
+    contentTemplate.innerHTML += `<a href="issue.html"><button type="submit" class="custom-button primary-button mx-2">Go to Issue</button></a>`;
     this.notification_list.appendChild(contentTemplate);
     this.addClassesToElementList(contentTemplate);
     this.notification_list.appendChild(contentTemplate);
-
   }
 }
 
@@ -216,15 +262,15 @@ class meeting extends message {
   }
 
   render() {
-    let contentTemplate = document.createElement('li');
+    let contentTemplate = document.createElement("li");
     contentTemplate.classList.add("meeting-notification");
-    let upperContent = document.createElement('div')
-    upperContent.classList.add('d-flex', 'justify-content-between');
+    let upperContent = document.createElement("div");
+    upperContent.classList.add("d-flex", "justify-content-between");
     upperContent.innerHTML += `<p><i class="fas fa-calendar-alt"></i> You have a meeting for <span class="project-reference">${this.project}</span> at ${this.date}</p>
                                 <p class="timestamp smaller-text">${this.date}</p>`;
 
-    contentTemplate.appendChild(upperContent)
-    contentTemplate.innerHTML += `<a href="calendar.html"><button type="submit" class="custom-button primary-button mx-2">Go to Calendar</button></a>`
+    contentTemplate.appendChild(upperContent);
+    contentTemplate.innerHTML += `<a href="calendar.html"><button type="submit" class="custom-button primary-button mx-2">Go to Calendar</button></a>`;
 
     this.addClassesToElementList(contentTemplate);
     this.notification_list.appendChild(contentTemplate);
@@ -233,6 +279,3 @@ class meeting extends message {
 
 /*calls */
 renderMessages(messages);
-
-
-
