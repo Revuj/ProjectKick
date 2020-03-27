2495b87990908ff97e2c6666566d4d6466ba1e9d
JoAPSo Paulo Gomes Torres Abelha <jpabelha@hotmal.com>
2020-03-27 14:33:33 +0000

    tools

---
+++ upload_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Stop execution if a step fails
+set -e
+
+DOCKER_USERNAME=lbaw2025 # Replace by your docker hub username
+IMAGE_NAME=lbaw2025-piu
+
+docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
+docker push $DOCKER_USERNAME/$IMAGE_NAME
+
