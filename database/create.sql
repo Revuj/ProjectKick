0f94848c870137020aa866ba6e16b3ceb2ca4dcc
João Rafael Varela <revuj112@gmail.com>
2020-03-29 00:14:56 +0000

    Update create sql script

--- database/create.sql
+++ database/create.sql
@@ -1,36 +1,31 @@
+CREATE TYPE country_name AS ENUM ('AF','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BA','BW','BR','IO','VG','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CK','CR','HR','CU','CW','CY','CZ','CD','DK','DJ','DM','DO','TL','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','PF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GU','GT','GG','GN','GW','GY','HT','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','CI','JM','JP','JE','JO','KZ','KE','KI','XK','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MR','MU','YT','MX','FM','MD','MC','MN','ME','MS','MA','MZ','MM','NA','NR','NP','NL','AN','NC','NZ','NI','NE','NG','NU','KP','MP','NO','OM','PK','PW','PS','PA','PG','PY','PE','PH','PN','PL','PT','PR','QA','CG','RE','RO','RU','RW','BL','SH','KN','LC','MF','PM','VC','WS','SM','ST','SA','SN','RS','SC','SL','SG','SX','SK','SI','SB','SO','ZA','KR','SS','ES','LK','SD','SR','SJ','SZ','SE','CH','SY','TW','TJ','TZ','TH','TG','TK','TO','TT','TN','TR','TM','TC','TV','VI','UG','UA','AE','GB','US','UY','UZ','VU','VA','VE','VN','WF','EH','YE','ZM','ZW');
+CREATE TYPE project_role AS ENUM ('coordinator', 'developer');
+
 -- Tables
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS country;
 DROP TABLE IF EXISTS member_status;
+DROP TABLE IF EXISTS project;
+DROP TABLE IF EXISTS issue_list;
+DROP TABLE IF EXISTS issue;
+DROP TABLE IF EXISTS "comment";
+DROP TABLE IF EXISTS channel;
 DROP TABLE IF EXISTS message;
-DROP TABLE IF EXISTS event_meeting;
-DROP TABLE IF EXISTS event_personal;
-DROP TABLE IF EXISTS "event";
-DROP TABLE IF EXISTS vote;
-DROP TABLE IF EXISTS report;
+DROP TABLE IF EXISTS notification;
 DROP TABLE IF EXISTS notification_kick;
 DROP TABLE IF EXISTS notification_invite;
 DROP TABLE IF EXISTS notification_assign;
 DROP TABLE IF EXISTS notification_event;
-DROP TABLE IF EXISTS notification;
-DROP TABLE IF EXISTS issue_tag;
-DROP TABLE IF EXISTS user_tag;
+DROP TABLE IF EXISTS "event";
+DROP TABLE IF EXISTS event_meeting;
+DROP TABLE IF EXISTS event_personal;
 DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS color;
-DROP TABLE IF EXISTS "comment";
-DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS vote;
+DROP TABLE IF EXISTS report;
+DROP TABLE IF EXISTS user_tag;
+DROP TABLE IF EXISTS issue_tag;
 DROP TABLE IF EXISTS assigned_user;
-DROP TABLE IF EXISTS issue;
-DROP TABLE IF EXISTS issue_list;
-DROP TABLE IF EXISTS project;
-DROP TABLE IF EXISTS "user";
-DROP TABLE IF EXISTS country;
-
--- Types
-DROP TYPE IF EXISTS country_name;
-DROP TYPE IF EXISTS project_role;
-
-CREATE TYPE country_name AS ENUM ('AF','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BA','BW','BR','IO','VG','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CK','CR','HR','CU','CW','CY','CZ','CD','DK','DJ','DM','DO','TL','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','PF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GU','GT','GG','GN','GW','GY','HT','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','CI','JM','JP','JE','JO','KZ','KE','KI','XK','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MR','MU','YT','MX','FM','MD','MC','MN','ME','MS','MA','MZ','MM','NA','NR','NP','NL','AN','NC','NZ','NI','NE','NG','NU','KP','MP','NO','OM','PK','PW','PS','PA','PG','PY','PE','PH','PN','PL','PT','PR','QA','CG','RE','RO','RU','RW','BL','SH','KN','LC','MF','PM','VC','WS','SM','ST','SA','SN','RS','SC','SL','SG','SX','SK','SI','SB','SO','ZA','KR','SS','ES','LK','SD','SR','SJ','SZ','SE','CH','SY','TW','TJ','TZ','TH','TG','TK','TO','TT','TN','TR','TM','TC','TV','VI','UG','UA','AE','GB','US','UY','UZ','VU','VA','VE','VN','WF','EH','YE','ZM','ZW');
-CREATE TYPE project_role AS ENUM ('coordinator', 'developer');
-
 
 CREATE TABLE country (
     id SERIAL PRIMARY KEY,
@@ -39,8 +34,9 @@ CREATE TABLE country (
 
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
-    email text UNIQUE CHECK (email LIKE '%@.%_') NOT NULL,
+    email text UNIQUE CHECK (email LIKE '%@%.%_') NOT NULL,
     username text UNIQUE NOT NULL,
+    name text NOT NULL,
     phone_number text UNIQUE,
     photo_path text,
     is_deleted bool DEFAULT false NOT NULL,
@@ -121,45 +117,6 @@ CREATE TABLE message (
                                                      ON UPDATE CASCADE
 );
 
-CREATE TABLE notification (
-    id SERIAL PRIMARY KEY,
-    name country_name NOT NULL,
-    "date" timestamp with time zone DEFAULT now() NOT NULL,
-    description text,
-    receiver_id integer NOT NULL REFERENCES "user" ON DELETE CASCADE
-                                                     ON UPDATE CASCADE,
-    sender_id integer NOT NULL REFERENCES "user"  ON DELETE CASCADE
-                                                     ON UPDATE CASCADE CHECK (sender_id <> receiver_id)
-);
-
-CREATE TABLE notification_kick (
-    notification_id integer PRIMARY KEY REFERENCES notification  ON DELETE CASCADE
-                                                     ON UPDATE CASCADE,
-    project_id integer NOT NULL REFERENCES notification ON DELETE CASCADE
-                                                     ON UPDATE CASCADE
-);
-
-CREATE TABLE notification_invite (
-    notification_id integer PRIMARY KEY REFERENCES notification ON DELETE CASCADE
-                                                     ON UPDATE CASCADE,
-    project_id integer NOT NULL REFERENCES notification ON DELETE CASCADE
-                                                     ON UPDATE CASCADE
-);
-
-CREATE TABLE notification_assign (
-    notification_id integer PRIMARY KEY REFERENCES notification ON DELETE CASCADE
-                                                     ON UPDATE CASCADE,
-    project_id integer NOT NULL REFERENCES notification ON DELETE CASCADE
-                                                     ON UPDATE CASCADE
-);
-
-CREATE TABLE notification_event (
-    notification_id integer PRIMARY KEY REFERENCES notification ON DELETE CASCADE
-                                                     ON UPDATE CASCADE,
-    project_id integer NOT NULL REFERENCES notification ON DELETE CASCADE
-                                                     ON UPDATE CASCADE
-);
-
 CREATE TABLE "event" (
     id SERIAL PRIMARY KEY,
     title text NOT NULL,
@@ -181,9 +138,48 @@ CREATE TABLE event_personal (
                                                      ON UPDATE CASCADE
 );
 
-CREATE TABLE color (
+CREATE TABLE notification (
     id SERIAL PRIMARY KEY,
-    rgb_code text CHECK (rgb_code LIKE '[0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F]') NOT NULL
+    "date" timestamp with time zone DEFAULT now() NOT NULL,
+    description text,
+    receiver_id integer NOT NULL REFERENCES "user" ON DELETE CASCADE
+                                                     ON UPDATE CASCADE,
+    sender_id integer NOT NULL REFERENCES "user"  ON DELETE CASCADE
+                                                     ON UPDATE CASCADE CHECK (sender_id <> receiver_id)
+);
+
+CREATE TABLE notification_kick (
+    notification_id integer PRIMARY KEY REFERENCES notification  ON DELETE CASCADE
+                                                     ON UPDATE CASCADE,
+    project_id integer NOT NULL REFERENCES project ON DELETE CASCADE
+                                                     ON UPDATE CASCADE
+);
+
+CREATE TABLE notification_invite (
+    notification_id integer PRIMARY KEY REFERENCES notification ON DELETE CASCADE
+                                                     ON UPDATE CASCADE,
+    project_id integer NOT NULL REFERENCES project ON DELETE CASCADE
+                                                     ON UPDATE CASCADE
+);
+
+CREATE TABLE notification_assign (
+    notification_id integer PRIMARY KEY REFERENCES notification ON DELETE CASCADE
+                                                     ON UPDATE CASCADE,
+    issue_id integer NOT NULL REFERENCES issue ON DELETE CASCADE
+                                                     ON UPDATE CASCADE
+);
+
+CREATE TABLE notification_event (
+    notification_id integer PRIMARY KEY REFERENCES notification ON DELETE CASCADE
+                                                     ON UPDATE CASCADE,
+    event_id integer NOT NULL REFERENCES "event" ON DELETE CASCADE
+                                                     ON UPDATE CASCADE
+);
+
+create table color (
+    id SERIAL PRIMARY KEY,
+    rgb_code VARCHAR NOT NULL,
+    CONSTRAINT CK_color CHECK (rgb_code ~ '^[a-fA-F0-9]{8}$')
 );
 
 
@@ -192,7 +188,7 @@ CREATE TABLE vote (
                                                      ON UPDATE CASCADE,
     comment_id integer  NOT NULL REFERENCES comment ON DELETE CASCADE
                                                      ON UPDATE CASCADE,
-    upvote boolean NOT NULL,
+    upvote boolean,
     
     PRIMARY KEY (
         user_id,
@@ -248,4 +244,3 @@ CREATE TABLE assigned_user (
         issue_id
     )
 );
-
