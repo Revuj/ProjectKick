045c32f22b24eef45d4b52116f1d99f63f1d83f0
Joao Varela <revuj@REVUJ-PC.localdomain>
2020-04-20 12:42:53 +0100

    Database seed working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
-    public function index($id) {
+    public function index($id)
+    {
+        $users = User::all();
+        dd($users);
         return view('pages.user.user');
     }
 
-    public function projects($id) {
+    public function projects($id)
+    {
         return view('pages.user.projects');
     }
 }
