637c9215710c71ef24272ae7b41a766246e6569e
Joao Varela <revuj@REVUJ-PC.localdomain>
2020-05-04 00:58:45 +0100

    Removed unnecessary files

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,10 +12,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-     // 'App\Card' => 'App\Policies\CardPolicy',
-      //'App\Item' => 'App\Policies\ItemPolicy'
-       'App\User' => 'App\Policies\UserPolicy',
-       'App\Project' => 'App\Policies\ProjectPolicy'
+        'App\User' => 'App\Policies\UserPolicy',
+        'App\Project' => 'App\Policies\ProjectPolicy',
     ];
 
     /**
